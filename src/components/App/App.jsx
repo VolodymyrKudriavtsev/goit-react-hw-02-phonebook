ae71fa79cc827dfed204216bc0d1f5d8f3b00951
Volodymyr Kudriavtsev <vladimirkud@ukr.net>
2023-01-22 01:48:54 +0200

    step-1 start

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,10 +1,35 @@
 import { Component } from 'react';
-// import { Container } from './App.styled';
+import { Container } from './App.styled';
 
 class App extends Component {
-  state = {};
+  state = {
+    contacts: [],
+    name: '',
+  };
+
   render() {
-    return console.log(this.state);
+    return (
+      <Container>
+        <form>
+          <h2>Phonebook</h2>
+          <label>
+            Name
+            <input
+              type="text"
+              name="name"
+              pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+              title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+              required
+            />
+            <button>Add contact</button>
+          </label>
+        </form>
+        <div>
+          <h3>Contacts</h3>
+          <ul></ul>
+        </div>
+      </Container>
+    );
   }
 }
 
