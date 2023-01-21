3e66a1bc1e92f92d0e86f67f46aafcf045d74d1d
Volodymyr Kudriavtsev <vladimirkud@ukr.net>
2023-01-22 01:11:56 +0200

    3-2-1-GO!

---
+++ src/components/App/App.jsx
@@ -0,0 +1,11 @@
+import { Component } from 'react';
+// import { Container } from './App.styled';
+
+class App extends Component {
+  state = {};
+  render() {
+    return console.log(this.state);
+  }
+}
+
+export default App;
